572fdf7e10a147de615954d008b1c51bad477203
suNrisEinMyeYes <iklown3@gmail.com>
2020-03-03 14:07:45 +0700

    init

---
+++ laba4/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace laba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.start = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.inputPrice = new System.Windows.Forms.NumericUpDown();
+            this.inputDays = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Buy = new System.Windows.Forms.Button();
+            this.Sell = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Money = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Possessions = new System.Windows.Forms.Label();
+            this.noMoney = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDays)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(546, 22);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(87, 26);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 105);
+            this.chart1.Name = "chart1";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Green;
+            series4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series4.IsValueShownAsLabel = true;
+            series4.LabelFormat = "f4";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(952, 366);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // inputPrice
+            // 
+            this.inputPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputPrice.Location = new System.Drawing.Point(116, 22);
+            this.inputPrice.Name = "inputPrice";
+            this.inputPrice.Size = new System.Drawing.Size(120, 26);
+            this.inputPrice.TabIndex = 2;
+            // 
+            // inputDays
+            // 
+            this.inputDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputDays.Location = new System.Drawing.Point(370, 22);
+            this.inputDays.Name = "inputDays";
+            this.inputDays.Size = new System.Drawing.Size(120, 26);
+            this.inputDays.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Initial price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(286, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Days:";
+            // 
+            // Buy
+            // 
+            this.Buy.Location = new System.Drawing.Point(745, 22);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(75, 25);
+            this.Buy.TabIndex = 6;
+            this.Buy.Text = "Buy";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // Sell
+            // 
+            this.Sell.Location = new System.Drawing.Point(846, 22);
+            this.Sell.Name = "Sell";
+            this.Sell.Size = new System.Drawing.Size(80, 25);
+            this.Sell.TabIndex = 7;
+            this.Sell.Text = "Sell";
+            this.Sell.UseVisualStyleBackColor = true;
+            this.Sell.Click += new System.EventHandler(this.Sell_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(474, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Money:";
+            // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Money.Location = new System.Drawing.Point(551, 70);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(18, 20);
+            this.Money.TabIndex = 9;
+            this.Money.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(727, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Possessions:";
+            // 
+            // Possessions
+            // 
+            this.Possessions.AutoSize = true;
+            this.Possessions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Possessions.Location = new System.Drawing.Point(849, 70);
+            this.Possessions.Name = "Possessions";
+            this.Possessions.Size = new System.Drawing.Size(18, 20);
+            this.Possessions.TabIndex = 11;
+            this.Possessions.Text = "0";
+            // 
+            // noMoney
+            // 
+            this.noMoney.AutoSize = true;
+            this.noMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.noMoney.ForeColor = System.Drawing.Color.Red;
+            this.noMoney.Location = new System.Drawing.Point(125, 70);
+            this.noMoney.Name = "noMoney";
+            this.noMoney.Size = new System.Drawing.Size(147, 20);
+            this.noMoney.TabIndex = 12;
+            this.noMoney.Text = "No money to buy!";
+            this.noMoney.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 483);
+            this.Controls.Add(this.noMoney);
+            this.Controls.Add(this.Possessions);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Money);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Sell);
+            this.Controls.Add(this.Buy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputDays);
+            this.Controls.Add(this.inputPrice);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDays)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown inputPrice;
+        private System.Windows.Forms.NumericUpDown inputDays;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.Button Sell;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Money;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Possessions;
+        private System.Windows.Forms.Label noMoney;
+    }
+}
+
---
+++ laba4/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba4
+{
+    public partial class Form1 : Form
+    {
+        const double k = 0.02;
+        Random rnd = new Random();
+        double price, _money = 200;
+        int days, i = 1, _possessions = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Interval = 4000;
+            Money.Text = _money + "$";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 1;
+
+            chart1.Series[0].Points.Clear();
+            
+
+            price = (double)inputPrice.Value;
+
+            days = (int)inputDays.Value;
+
+            chart1.Series[0].Points.AddXY(0, price);
+
+            timer1.Start();
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            if (_money>= price)
+            {
+                noMoney.Visible = false;
+                _money -= price;
+                Money.Text = _money + "$";
+                _possessions ++;
+                Possessions.Text = _possessions.ToString();
+            }
+            else
+            {
+                noMoney.Visible = true;
+            }
+        }
+
+        private void Sell_Click(object sender, EventArgs e)
+        {
+            if (_possessions>0)
+            {
+                _money += price;
+                _possessions--;
+                Money.Text = _money + "$";
+                Possessions.Text = _possessions.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            price = price * (1 + k * (rnd.NextDouble() - 0.5));
+
+            chart1.Series[0].Points.AddXY(i, price);
+
+            i++;
+            if (i == days) timer1.Stop();
+        }
+    }
+}
